6918e36f06ff53ff4170aa010cb442cc34947dcf
Uoptsuu <102488800+Uoptsuu@users.noreply.github.com>
2023-04-20 19:02:41 +0700

    commit

--- view/site/index.php
+++ view/site/index.php
@@ -1,15 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <title>PetLover - Pet Care Website Template</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="Free HTML Templates" name="keywords">
-    <meta content="Free HTML Templates" name="description">
-    <?php include("layout/asset_header.php")?>
-</head>
-
+<?php $title = "Trang chủ";?>
+<?php include("layout/asset_header.php")?>
 <body>
     <!-- Topbar Start -->
     <div class="container-fluid">
@@ -46,21 +38,24 @@
         <div class="row py-3 px-lg-5">
             <div class="col-lg-4">
                 <a href="" class="navbar-brand d-none d-lg-block">
-                    <h1 class="m-0 display-5 text-capitalize"><span class="text-primary">Pet</span>Lover</h1>
+                    <h1 class="m-0 display-5 text-capitalize">
+                        <span class="text-primary">Care</span>PET | 
+                        <span style="font-size:20px">Hệ thống chăm sóc thú cưng số 1 HUCE</span>
+                    </h1>
                 </a>
             </div>
             <div class="col-lg-8 text-center text-lg-right">
                 <div class="d-inline-flex align-items-center">
                     <div class="d-inline-flex flex-column text-center pr-3 border-right">
-                        <h6>Opening Hours</h6>
+                        <h6>Giờ mở cửa</h6>
                         <p class="m-0">8.00AM - 9.00PM</p>
                     </div>
                     <div class="d-inline-flex flex-column text-center px-3 border-right">
-                        <h6>Email Us</h6>
-                        <p class="m-0">info@example.com</p>
+                        <h6>Email liên hệ</h6>
+                        <p class="m-0">carepet@huce.com</p>
                     </div>
                     <div class="d-inline-flex flex-column text-center pl-3">
-                        <h6>Call Us</h6>
+                        <h6>Số điện thoại</h6>
                         <p class="m-0">+012 345 6789</p>
                     </div>
                 </div>
