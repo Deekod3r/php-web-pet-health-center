4694a3f3bed851fff9a51fefa31c61ecd4c13ddb
Uoptsuu <102488800+Uoptsuu@users.noreply.github.com>
2023-04-20 10:29:56 +0700

    commit

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -1,5 +1,6 @@
 <?php
 class BaseController {
+
     public function renderView($view_name, $data=[]) {
         extract($data);
         include('views/'.$view_name.'.php');
@@ -12,5 +13,14 @@ class BaseController {
     }
     public function redirect($controller, $action) {
         header("Location: ?controller=" . $controller . "&action=" . $action);
+    }
+
+    public function __construct()
+    {
+        session_start();
+        if(!isset($_SESSION['is_login'])) {
+            $_SESSION['error_login_message'] = "Vui lòng đăng nhập để sử dụng này.";
+            $this->redirect('Home', 'login_page');
+        }
     }
 }
--- repository/BaseRepository.php
+++ repository/BaseRepository.php
@@ -1,8 +1,13 @@
 <?php 
+
+use Response;
+use Constant;
 class BaseRepository {
 
     protected $table;
+    protected $id_table;
 
+    private $constant = new Constant();
     protected function getConnection() {
         $hostname = "localhost";
         $username = "root";
@@ -17,15 +22,81 @@ class BaseRepository {
     }
 
     protected function findAll(){
-        global $table;
         $query = "SELECT * FROM " . $this->table;
-        $result = $this->getConnection()->query($query);
-        $data = [];
-        if($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $data[] = $row;
-            }
+        $result = null;
+        $response = null;
+        try{
+            $result = $this->getConnection()->query($query);
+            $data = [];
+            if($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $data[] = $row;
+                }
+                $response = new Response(uniqid($this->constant::RESPONSE_SEARCH), $this->constant::SEARCH_MESSAGE_SUCCESS, $data, 200);
+            } else throw new Exception();
+        } catch(Exception $e){
+            $response = new Response(uniqid($this->constant::RESPONSE_SEARCH),$this->constant::SEARCH_MESSAGE_FAIL,$result,501);
         }
-        return $data;
+        return $response;
     }
-}
+
+    protected function findById($id){
+        $query = "SELECT * FROM " . $this->table . " WHERE $this->id_table = " . $id;
+        $result = null;
+        $response = null;
+        try{
+            $result = $this->getConnection()->query($query);
+            $data = [];
+            if($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $data[] = $row;
+                }
+                $response = new Response(uniqid($this->constant::RESPONSE_SEARCH), $this->constant::SEARCH_MESSAGE_SUCCESS, $data, 200);
+            } else throw new Exception();
+        }catch(Exception $e){
+            $response = new Response(uniqid($this->constant::RESPONSE_SEARCH),$this->constant::SEARCH_MESSAGE_FAIL,null,501);
+        }
+        return $response;
+    }
+
+    protected function save($data){
+        $query = "Insert into " . $this->table . " values(" . implode(",", $data) . ")";
+        $response = null;
+        try {
+            if($this->getConnection()->query($query)){
+                $response = new Response(uniqid($this->constant::RESPONSE_INSERT),$this->constant::INSERT_MESSAGE_SUCCESS,true,200);
+            } else throw new Exception();
+        } catch (Exception $e){
+            $response = new Response(uniqid($this->constant::RESPONSE_INSERT),$this->constant::INSERT_MESSAGE_FAIL,true,501);        
+        }
+        return $response;
+    }
+
+    protected function delete($id){
+        $query = "Delete from $this->table where $this->id_table = " . $id;
+        $response = null;
+        try {
+            if($this->getConnection()->query($query)){
+                $response = new Response(uniqid($this->constant::RESPONSE_DELETE),"Delete success.",true,200);
+            } else throw new Exception("Delete fail.");
+        } catch (Exception $e){
+            $response = new Response(uniqid($this->constant::RESPONSE_DELETE),$e->getMessage(),true,501);        
+        }
+        return $response;
+    }
+
+    protected function update($data, $id){
+        $query = "Delete from $this->table where $this->id_table = " . $id;
+        $response = null;
+        try {
+            if($this->getConnection()->query($query)){
+                $response = new Response(uniqid($this->constant::RESPONSE_DELETE),"Delete success.",true,200);
+            } else throw new Exception("Delete fail.");
+        } catch (Exception $e){
+            $response = new Response(uniqid($this->constant::RESPONSE_DELETE),$e->getMessage(),true,501);        
+        }
+        return $response;
+    }
+
+    
+}   
