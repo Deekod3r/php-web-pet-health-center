03a269fdd91dd63518f1df156ce4f6bbf7af7d1a
Uoptsuu <102488800+Uoptsuu@users.noreply.github.com>
2023-04-20 10:55:26 +0700

    commit

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -3,24 +3,28 @@ class BaseController {
 
     public function renderView($view_name, $data=[]) {
         extract($data);
-        include('views/'.$view_name.'.php');
+        $path = 'view/';
+        if(isset($_SESSION["admin"]) && $_SESSION["admin"]){
+            $path .= 'admin/';
+        } else $path .= 'site/';
+        include($path.$view_name.'.php');
     }
 
-    public function getModel($model_name) {
-        include('models/'.$model_name.'.php');
-        $modelObj = new $model_name();
-        return $modelObj;
+    public function getModel($service_name) {
+        include('service/'.$service_name.'Service.php');
+        $serviceObj = new $service_name();
+        return $serviceObj;
     }
     public function redirect($controller, $action) {
         header("Location: ?controller=" . $controller . "&action=" . $action);
     }
 
-    public function __construct()
-    {
-        session_start();
-        if(!isset($_SESSION['is_login'])) {
-            $_SESSION['error_login_message'] = "Vui lòng đăng nhập để sử dụng này.";
-            $this->redirect('Home', 'login_page');
-        }
-    }
+    // public function __construct()
+    // {
+    //     session_start();
+    //     if(!isset($_SESSION['is_login'])) {
+    //         $_SESSION['error_login_message'] = "Vui lòng đăng nhập để sử dụng này.";
+    //         $this->redirect('Home', 'login_page');
+    //     }
+    // }
 }
--- routes.php
+++ routes.php
@@ -1,19 +1,16 @@
 <?php
-// include('controllers/BaseController.php');
-// include('controllers/AdminController.php');
-// include('models/BaseModel.php');
-
+include('controller/BaseController.php');
 if(isset($_GET['controller']) && $_GET['action']) {
+    //echo $_GET['controller']. " " . $_GET['action'];
     $controller = $_GET['controller'];
     $action = $_GET['action'];
-    $controllerFileName = $controller.'Controller.php';
-    $controllerFileName = 'controllers/'.$controllerFileName;
+    $controllerFileName ='controller/'.$controller.'Controller.php';
     if(file_exists($controllerFileName)) {
         include($controllerFileName);
         $controllerClass = $controller.'Controller';
         $controllerObj = new $controllerClass();
         $controllerObj->$action();
     }else {
-        include('views/404-notfound.php');
+        include('view/error/error.php');
     }
 }
