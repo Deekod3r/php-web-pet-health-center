02e152cabe6b7746c12002028c62750bdf12181f
ThuyDung1911 <103367661+ThuyDung1911@users.noreply.github.com>
2023-05-20 19:29:01 +0700

    a

--- database/02_demo_data.sql
+++ database/02_demo_data.sql
@@ -23,11 +23,6 @@ insert into customer (ctm_id, ctm_name, ctm_phone, ctm_email, ctm_address, ctm_p
                    (12,'Hoàng Quốc Việt','0987126859','vitvang2000@gmail.com','Nguyễn Hiền, Hai Bà Trưng, Hà Nội','viet.2000',1, true, false);
 
 -- select * from customer;
-
-update customer set ctm_password = 'c4ca4238a0b923820dcc509a6f75849b' where ctm_active = 1; # pass: 1
-
-update admin set ad_password = 'c4ca4238a0b923820dcc509a6f75849b' where 1=1; # pass: 1
-
 -- discount
 insert into discount (dc_id, dc_code, dc_description, dc_condition, dc_value, dc_value_percent, dc_start_time, dc_end_time, dc_quantity, dc_active, is_delete)
             values (1, 'HAPPYPET','Mã giảm giá ngày khai trương',default,50000,default,'2023/03/15','2023/03/22',null,1,default),
@@ -66,31 +61,68 @@ insert into news (news_id, news_title, news_description, news_content, news_img,
             values (default,'Bật Mí Cách Nuôi Chó Corgi Hiệu Quả Cho Người Chưa Có Kinh Nghiệm P1',
                     'Thức ăn cho chó Corgi Để cún cưng Corgi có thể phát triển khỏe mạnh, Siêu Pet khuyên bạn nên chú ý đến chế độ dinh dưỡng cùng khẩu phần hợp lý mỗi ngày. Vì thức ăn đóng vai trò quan trọng trong sự phát triển toàn diện của cảnh khuyển.',
                     'Chế độ dinh dưỡng của Corgi Sở hữu thân hình nhỏ bé nhưng khá tinh nghịch, Corgi đòi hỏi bạn phải cung cấp đầy đủ các chất dinh dưỡng. Theo đó, các chất bắt buộc phải có trong khẩu phần ăn của cún là: Protein: Có trong các loại thịt, nhiều nhất là thịt bò, sau đó là đến thịt gà, thịt lợn, trứng vịt lộn,… Chất béo: Thường đã có sẵn trong các loại thịt chứa protein, bạn không cần cần bổ sung thêm vì dễ khiến Corgi bị béo phì. Chất xơ: Trong các loại rau củ quả, trong đó tốt nhất vẫn là cà rốt. Chất xơ cực kì tốt cho hệ tiêu hóa và bộ lông của Corgi. Vitamin, khoáng chất: Các loại vitamin thường có nhiều trong thực phẩm tươi như: Cà rốt, tôm, ngao, ốc,… Tinh bột: Có nhiều trong cơm, cháo, khoai tây, khoai lang, sắn,… Khẩu phần ăn của Corgi theo độ tuổi Corgi từ 1-2 tháng tuổi: Giai đoạn này hê tiêu hóa của cún còn rất kém. Bạn nên chú ý cho chúng dùng thức ăn mềm như: Cháo loãng, sữa, thức ăn khô ngâm mềm, khoai tây nghiền,… Đồng thời, bạn nên cho Corgi ăn thành nhiều bữa: Khoảng 4-5 bữa + 200ml sữa ấm một ngày. Corgi từ 3-6 tháng tuổi: Corgi tuổi này đã bắt đầu ăn được thịt, tôm, cá, rau,… Nhưng thức ăn cũng phải được cắt nhỏ và làm mềm, đề phòng Corgi bị hóc. Bạn có thể giảm xuống còn 3-4 bữa một ngày, sữa thì nên uống 200-300ml. Corgi trên 6 tháng tuổi: Cún Corgi lúc này đang vào độ tuổi phát triển mạnh nhất. Bạn nên cho chúng ăn đa dạng các loại thức ăn từ thịt, tôm, cá đến khoai, mì,… để cung cấp đầy đủ các chất. Ở giai đoạn này, bạn chỉ cần cho cún ăn 2-3 bữa một ngày, sữa có thể uống có thể không. Nếu muốn bộ lông của Corgi bóng mượt thì bạn có thể cho ăn 2-3 quả trứng vịt lộn mỗi tuần.',
-                    '#','2023/03/15',1,4,3,false),
+                    'view/upload/admin/news/nuoi-cho-corgi1.jpg','2023/03/15',1,4,3,false),
                     (default,'Hướng dẫn cách nuôi mèo Anh lông ngắn chi tiết cho người mới',
                     'Mèo Anh lông ngắn luôn nằm trong top những giống mèo đẹp nhất trên thế giới. Giống mèo này rất được ưa chuộng để lựa chọn làm thú cưng nhờ thân hình mũm mĩm, lông ngắn, khuôn mặt bầu bĩnh và tính cách thân thiện. Tuy nhiên, để chăm sóc cho chúng khỏe mạnh, ngoan ngoãn thì không phải ai cũng biết cách.' ,
                     'Các vấn đề, bệnh lý thường gặp Giống mèo Anh lông ngắn thường gặp những chứng bệnh đặc biệt, vì thế bạn cần tìm hiểu kỹ càng nguyên nhân để tìm ra phương pháp chữa trị đúng cách. Các loại bệnh mà mèo Anh lông ngắn thường hay mắc phải như: Bệnh tiểu đường Bệnh viêm nướu răng Bệnh Feline Leukemia Virus Bệnh dại Bệnh nhiễm trùng đường hô hấp Bệnh Chlamydia Bệnh ký sinh trùng Bệnh Hypertrophic Cardiomyopathy',
-                    '#','2023/03/25',1,4,2, false),
+                    'view/upload/admin/news/nuoi-cho-corgi2.jpg','2023/03/25',1,4,2, false),
                     (default,'Bật Mí Cách Nuôi Chó Corgi Hiệu Quả Cho Người Chưa Có Kinh Nghiệm P2',
                     'Corgi là giống cảnh khuyển xứ lạnh nên chúng khó có thể phát triển toàn diện tại khí hậu nắng nóng của Việt Nam. Trong quá trình nuôi dưỡng cún, bạn cần lưu ý đến cách chăm sóc và vệ sinh để Corgi có thể phòng tránh một số bệnh không đáng có.',
                     'Sở hữu bộ lông dày 2 lớp với tác dụng giữ nhiệt nên khi du nhập vào Việt Nam, công dụng này không còn phù hợp với cún. Để Corgi có thể thoải mái phát triển, Siêu Pet nhắc bạn nên chú ý cắt tỉa lông khoảng 2-3 tháng một lần. Ngoài việc cắt tỉa, bạn cũng nên tắm cho cún thường xuyên. Đồng thời, dùng lược chải lông cho Corgi mỗi ngày để loại bỏ phần lông chết. Nếu lông Corgi ẩm ướt, bạn nên dùng máy sấy sấy khô, tránh để vi khuẩn có cơ hội phát triển. Nếu không có thời gian chăm sóc lông cho Corgi, bạn có thể đưa chúng đến các Spa thú cưng.',
-                    '#','2023/03/15',1,4,4,false),
+                    'view/upload/admin/news/nuoi-cho-corgi3.jpg','2023/03/15',1,4,4,false),
                     (default,'Bật Mí Cách Nuôi Chó Corgi Hiệu Quả Cho Người Chưa Có Kinh Nghiệm P3',
                     'Corgi thuộc vào nhóm những giống cảnh khuyển có tuổi thọ khá cao: Khoảng 12-15 năm. Để cún cưng được khỏe mạnh với số tuổi lâu hơn, bạn nên thực hiện tiêm phòng cho chúng định kỳ.',
                     'Vaccine dành cho Corgi thường có 3 loại là: 3 in 1, 5 in 1 và 7 in 1. Trong đó, loại 3 trong 1 bây giờ ít khi được sử dụng do có hiệu quả không cao. Các phòng khám thú y thường khuyên bạn dùng 2 loại 5 trong 1 và 7 trong 1. Các loại vaccine đó có thể phòng ngừa một số bệnh như: Care, Pravo, gan truyền nhiễm, cúm,… Theo kinh nghiệm của Siêu Pet, bạn nên bắt đầu tiêm phòng cho Corgi từ khi còn nhỏ, khoảng 3 tuần tuổi là bắt đầu mũi thứ nhất. Sau đó, liệu trình sẽ kéo dài đến khi chúng khoảng 3 tháng tuổi là kết thúc mũi cuối. Giá vaccine cũng không đắt, chỉ từ 120k – 250k một mũi, tùy từng loại thuốc. Khi cảnh khuyên chân ngắn Corgi được 8 tháng tuổi thì nên được tiêm phòng dại. Khi tiêm phòng thì bạn cũng nên hỏi các bác sĩ cho cún uống thuốc tẩy giun sán đều đặn.',
-                    '#','2023/03/15',1,4,2,false),
+                    'view/upload/admin/news/','2023/03/15',1,4,2,false),
                     (default,'Mèo mẹ nhờ cô chủ trông hộ con',
                     'Cô chủ than: ''Đêm nào nó cũng tha con nó vào cho tui coi để nó đi chơi''.' ,
                     'Mèo mẹ nhờ cô chủ trông hộ con. Đêm nào nó cũng tha con nó vào cho cô chủ coi để nó đi chơi ',
-                    'https://video.vnexpress.net/embed/v_376038','2023/03/25',1,5,5, false),
+                    'view/upload/admin/news/meo-nho-trong-con.jpg','2023/03/25',1,5,5, false),
                     (default,'10 biện pháp ''kịch độc'' đối phó chó thả rông',
                     'Để đối phó với nạn chó thả rông, bạn đọc đề nghị: Chỉ được phép nuôi các loại chó có trọng lượng... dưới 1kg! Nếu không thì phải là loại chó... không răng nhé!',
                     'Khi các biện pháp không giải quyết được chó thả rông, có lẽ đành phải trông vào... thế lực siêu nhiên thôi: trước khi ra đường cứ bói một quẻ hung cát cho chắc, để xem hướng nào có chó dữ thì tránh đi - bạn NGUYỄN NGỌC A (Phú Yên) đề xuất.',
-                    '#','2023/04/1',1,5,6,false),
+                    'view/upload/admin/news/cho-tha-rong.jpg','2023/04/1',1,5,6,false),
                     (default,'CarePet - Cùng bạn mang yêu thương chăm sóc thú cưng.',
                     'Chúng tôi luôn đồng hành, thấu hiểu những nổi lo lắng của bạn trong quá trình chăm lo, nuôi nấng thúcưng của bạn. Pet care ra đời với sứ mệnh người bạn-  người đồng hành luôn sát cánh cùng bạn nuôi dưỡng và  mang lại dịch vụ tốt nhất làm hài lòng khách hàng và thú cưng.về cửa hàng CarePet chúng tôi cung cấp dịch vụ chăm sóc, khám bệnh cho thú cưng.',
                     'Bất kì một doanh ngiệp nào ra đời cũng muốn xây dựng, phát triển thành một thương hiệu vững mạnh, có chỗ đứng trong lòng người tiêu dùng. CAREPET cũng vậy, chúng tôi luôn nổ lực hết mình trong việc cung cấp những sản phẩm, dịch vụ chất lượng nhất đến với khách hàng của mình, với tiêu chí ‘ khách hàng là trên hết’. Chúng tôi coi sự hài lòng của khách hàng là một trong những mục tiêu quan trọng hàng đầu của việc kinh doanh. Chúng tôi tin tưởng rằng trong một ngày nào không xa chúng tôi sẻ trở thành một thương hiệu hiệu lớn và co mặt trong mỗi một gia đình ',
-                    '#','2023/04/1',1,5,1,false);
+                    'view/upload/admin/news/carepet.jpg','2023/04/1',1,5,1,false),
+                    (default,'[ HÉ LỘ ] Cách trị chó bị viêm da #Hiệu quả #Chính xác',
+                    'Bạn đang đau đầu khi không biết liệu có cách trị chó bị viêm da? Tuy bệnh viêm da không làm ảnh hưởng quá nghiêm trọng tới sức khỏe của cún cưng nhưng sẽ khiến chó con rất khó chịu, gây ngứa ngáy và đặc biệt là sẽ có mùi rất hôi hám. Và chắc chắn bạn sẽ không muốn ôm một chú chó như thế đúng không nào? Trong bài viết này, Kimi Pet sẽ cùng bạn tìm hiểu cách chữa trị cũng như cách phòng ngừa khi cún cưng bị bệnh viêm da nhé!',
+                    '1. Nguyên nhân chó bị viêm da
+                    Bệnh viêm da ở chó là tình trạng da bị viêm, ngứa. Chó cưng của bạn sẽ cảm thấy khó rất khó chịu khi mắc phải chứng bệnh này. Trong quá trình vận động hàng ngày, việc chú chó hay gãi, liếm và cắn liên tục sẽ khiến da đỏ, đau đớn và dễ bị nhiễm trùng. Bệnh này thường xuất hiện ở vùng bàn chân, bụng hoặc nách của chó.
+                    Có rất nhiều nguyên nhân gây ra bệnh viêm da nhưng chủ yếu là do:
+                    Do ký sinh trùng gây ra như bọ chét, ve,…
+                    Do bị nhiễm trùng bởi các loại vi khuẩn hoặc nấm,…
+                    Do bị dị ứng với Môi trường hoặc từ thức ăn.',
+                    'view/upload/admin/news/cho-viem-da.png','2023/05/10',1,5,2,false),
+                    (default,'TOP 6+ Nguyên nhân chó bị rụng lông | Nhất định phải biết',
+                    'Bạn có biết tại sao chó bị rụng lông chưa? Việc lông cún cưng rơi khắp nhà khiến bạn vô cùng đau đầu? Cùng CarePet tìm hiểu 6+ nguyên nhân tại sao và cách giải quyết khi chó rụng lông quá nhiều trong bài viết dưới đây nhé.',
+                    'Đa số các giống chó thay lông 2 lần trong 1 năm.
+                    Lần đầu là cuối đông, khi nhiệt độ bắt đầu ấm dần, chó rụng bớt lớp lông dày để hạ nhiệt.
+                    Lần thứ hai là cuối thu, trời chuyển lạnh, chó cần thay lớp lông mới để giữ ấm. Thông thường là tháng 3-5 và tháng 9-11.
+                    Ở các giống chó lông dài, tình trạng rụng lông càng nghiêm trọng. Có đôi khi chỉ cần vuốt dọc sống lưng là chó rụng lông một búi. Về mùa hè, lông chó thường tơi xốp, ngắn và mềm hơn, giúp giảm bớt nhiệt lượng. Bộ lông còn có tác dụng bảo vệ làn da, tránh tổn thương do tia tử ngoại từ ánh nắng mặt trời.
+                    Mùa đông lớp lông cần dày và nặng để giữ nhiệt độ bên trong cơ thể. Bộ lông mùa đông thường thô cứng bên ngoài và mềm ở bên trong. Giúp chó không bị hạ nhiệt, cảm lạnh. Đồng thời bảo vệ chó khỏi những tia bức xạ có hại từ ánh nắng mặt trời.',
+                    'view/upload/admin/news/cho-rung-long.jpg','2023/05/12',1,5,4,false),
+                    (default,'[MÁCH BẠN] Chó Poodle có mấy size #Chi Tiết và #CHUẨN Nhất #2023',
+                    'Chó Poodle có mấy loại chắc hẳn không phải ai cũng biết. Mặc dù đây là giống chó rất được ưa thích và phổ biến ở Việt Nam. Care Pet sẽ giúp bạn trả lời câu hỏi chó poodle có bao nhiêu loại ở bài viết dưới.',
+                    'Theo một số tài liệu ghi chép thì Poodle là giống chó xuất xứ từ vùng Trung Á. Mặc dù vậy, vẫn có nhiều nơi trên thế giới có sự xuất hiện của loài chó này như Pháp, Nga và Hungary.
+                    “Poodle” ở mỗi quốc gia sẽ có một ý nghĩa khác nhau. Poodle ở tiếng đức có nghĩa là Pudel, có nghĩa là thợ lặn. Ở Pháp, chúng được gọi với cái tên khác là Caniche, có nghĩa là chó săn vịt.',
+                    'view/upload/admin/news/poodle-size.png','2023/05/15',1,5,6,false),
+                    (default,'[GIẢI ĐÁP] Chó Poodle mang thai mấy tháng thì đẻ #Mới nhất #2023',
+                    'Không phải ai cũng biết chó poodle mang thai bao lâu thì đẻ, mặc dù giống chó Poodle được rất nhiều người ưa thích và nhiều hộ gia đình cũng nuôi loài chó này. Với bài viết dưới Care Pet sẽ có những giải đáp các vấn đề xung quanh việc chó poodle mang thai bao nhiêu ngày thì đẻ.',
+                    'Câu trả lời là dao động khoảng từ 58-68 ngày. Điều này sẽ tùy thuộc vào cơ địa của chó poodle nhà bạn.
+                    Chó poodle giống cái thường động dục ở độ tuổi từ 6-8 tháng. Ở giai đoạn này, bộ phận sinh dục của chó sẽ nở ra và có phần nhô ra ngoài, tiếp đó là chảy máu đi kèm với dịch nhầy. Bạn hãy lưu ý cho các bé phối giống vào thời điểm thích hợp.
+                    Nếu phối giống thành công, thể hiện qua các dấu hiệu và siêu âm, poodle sẽ mang thai.
+                    Cách chăm sóc chó mang thai giai đoạn thai kỳ là một vấn đề phức tạp. Bạn không chỉ cần đầu tư công sức, thời gian, chi phí mà còn phải thật sự yêu thương và quan tâm chúng.',
+                    'view/upload/admin/news/poodle-mang-thai.jpeg','2023/05/18',1,5,6,false),
+                    (default,'[CHIA SẺ] Top 9 cách tìm mèo đi lạc đơn giản hiệu quả tốt nhất',
+                    'Tìm hiểu các cách tìm mèo lạc giúp đưa mèo cưng của bạn về nhà một cách nhanh chóng. Hãy cùng Care Pet tìm hiểu cách tìm lại mèo đi lạc chi tiết trong bài viết này nhé.',
+                    'Các bạn có thể thử áp dụng cách gọi mèo lạc về tâm linh. Lưu ý, đây chỉ là cách làm theo dân gian, không đảm bảo thành công 100%. Bạn có thể thực hiện các bước như sau:
+                    Bước 1: Lấy một bát nước sạch để lên trên mặt bếp hoặc đặt ở giữa 2 vòng lửa của bếp ga, trên bát để một cái kéo. Nếu bạn không có bếp thì để bát ở trên bàn gần nơi có lỗ thông gió hoặc cửa ra vào.
+                    Bước 2: Hãy mở kéo ra và để mũi kéo hướng về phía cửa nhà hoặc cửa sổ, sau đó gọi to tên mèo của bạn.
+                    Bước 3: Nếu mèo về nhà được thì phải cầm 4 chân mèo rồi đi quanh bàn hoặc bếp 3 vòng để bày tỏ lòng cảm ơn.
+                    Lưu ý, trong trường hợp mèo chưa về thì không được dịch chuyển bát và kéo, bởi mỗi lần dịch chuyển sẽ làm mất hết hiệu quả và phải chờ lại từ đầu.',
+                    'view/upload/admin/news/meodilac.jpg','2023/05/20',1,5,6,false);
 -- select * from news;
 -- category_service
 insert into category_service (cs_id, cs_name, is_delete)
@@ -146,7 +178,7 @@ insert into service (sv_id, sv_name, sv_img, sv_price, sv_description, sv_pet, s
                    (16,'2023/05/18','15:45:00','2023/05/16 8:39:00',null,0,3,3,false);
 -- select * from appointment;
 -- bill
-insert into bill (bill_id, bill_date_release, bill_status, is_delete, ctm_id, ad_id, dc_id, sub_total, value_reduced, total_value)
+insert into bill (bill_id, bill_date_release, bill_status, is_delete, ctm_id, ad_id, dc_id, value_temp, value_reduced, total_value)
             values (1,'2023/03/16',1,default,1,3,1,200000,50000,150000), -- kh1, giảm 50k vs đơn tối thiểu 0đ
                    (2,'2023/04/10',1,default,3,2,null,500000,0,500000), -- kh3 ko đc giảm giá
                    (3,'2023/04/01',1,default,1,2,null,250000,0,250000),
